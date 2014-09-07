5a68ec7153238d358315b35ff0b38a82e673373f
softvar <varun2902@gmail.com>
2014-09-07 16:19:01 +0530

    Updated fabfile for automating tasks

--- fabfile.py
+++ fabfile.py
@@ -15,12 +15,3 @@ def serve():
 def cgs():
   regen()
   serve()
- 
-@hosts('jwmcmanus@jmcman.us')
-def publish():
-  regen()
-  project.rsync_project(
-    remote_dir='/home/jwmcmanus/jmcman.us',
-    local_dir='deploy/',
-    delete=True
-  )
